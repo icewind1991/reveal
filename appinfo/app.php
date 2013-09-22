e99b947ae454de8fc0ff363707f10d00777222e4
Robin Appelman <icewind@owncloud.com>
2013-09-22 19:19:59 +0200

    add reveal template

--- appinfo/app.php
+++ appinfo/app.php
@@ -23,10 +23,10 @@
 
 namespace OCA\Reveal;
 
-\OCP\Util::addscript( 'reveal', 'public/editor');
+\OCP\Util::addscript('reveal', 'public/editor');
 
-\OCP\App::addNavigationEntry( array(
-	
+\OCP\App::addNavigationEntry(array(
+
 	// the string under which your app will be referenced
 	// in owncloud, for instance: \OC_App::getAppPath('APP_ID')
 	'id' => 'reveal',
@@ -34,15 +34,18 @@ namespace OCA\Reveal;
 	// sorting weight for the navigation. The higher the number, the higher
 	// will it be listed in the navigation
 	'order' => 74,
-	
+
 	// the route that will be shown on startup
 	'href' => \OCP\Util::linkToRoute('reveal_index'),
-	
+
 	// the icon that will be shown in the navigation
-	'icon' => \OCP\Util::imagePath('reveal', 'example.png' ),
-	
+	'icon' => \OCP\Util::imagePath('reveal', 'example.png'),
+
 	// the title of your application. This will be used in the
 	// navigation or on the settings page of your app
 	'name' => \OC_L10N::get('reveal')->t('Reveal.js')
-	
+
 ));
+
+$templateManager = \OC_Helper::getFileTemplateManager();
+$templateManager->registerTemplate('text/reveal', __DIR__ . '/../templates/template.reveal');
